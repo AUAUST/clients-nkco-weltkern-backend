98620ce4f642b76524f3bfb84b3ea8e25105b95b
apademide <contact@apademide.ch>
2023-03-28 13:47:56 +0200

    style: remove debug comments

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,13 +1,5 @@
 <small>
-
   <?php dump($page->title()) ?>
 </small>
 <p>
-  <?php
-  $string =
-    "Il l'avait: dit; à moi! \"C'est moi\", qu'il avait dit. - `simple` -- au --- `double` (C'est moi.) [C'est moi.] {C'est moi.} Pourquoi? demanda-t-il.";
-  echo ($string);
-  echo ("<br><br><br>");
-  echo ($kirby->smartypants($string))
-  ?>
 </p>
