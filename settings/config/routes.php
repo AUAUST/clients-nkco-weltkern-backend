f415ab86632606877876dcbdab9293cad8ca386f
apademide <contact@apademide.ch>
2023-05-04 16:36:41 +0200

    imp: route renders the product template when found

--- settings/config/routes.php
+++ settings/config/routes.php
@@ -5,7 +5,8 @@ use Kirby\Cms\Response;
 return [
   [
     'pattern' => ['product', 'product/(:any)'],
-    'action'  => function ($id = null) {
+    'language' => '*',
+    'action'  => function ($lang = "", $id = null) {
 
       // Get the products
       $products = page('products')->children();
@@ -20,28 +21,12 @@ return [
         $product = $products->find($id);
       }
 
-      // Define the result as the product if it exists
+      // Render the product if it exists
       if ($product) {
-        $result = [
-          "success" => true,
-          "product" => [
-            "id" => array_reverse(explode("/", $product->id())),
-            "uuid" => $product->uuid()->model()->content()->uuid()->toString(),
-            "name" => $product->title()->toString(),
-            "author" => $product->author()->toString(),
-          ]
-        ];
+        return site()->visit($product, $lang);
       }
 
-      // Define the result as not found if the product doesn't exist
-      else {
-        $result = [
-          "success" => false,
-        ];
-      }
-
-      // Return the result as JSON
-      return Response::json($result, null, true);
+      throw new Exception('The product "' . $id . '" does not exist');
     }
   ],
 ];
