6f8d9452aa7dc9988d6347caf09d3ff84c0de352
apademide <contact@apademide.ch>
2023-05-04 13:39:38 +0200

    feat: API get products by ID or UUID

--- settings/config/routes.php
+++ settings/config/routes.php
@@ -7,22 +7,40 @@ return [
     'pattern' => ['product', 'product/(:any)'],
     'action'  => function ($id = null) {
 
-      $productsPage = page('products');
-      $product = $productsPage->children()->find($id);
+      // Get the products pages
+      $products = page('products')->children();
 
+      // Try to find the product by id if it matches the UUID format
+      if (preg_match('/^[a-zA-Z0-9]{16}$/', $id)) {
+        $product = $products->find("page://" . $id);
+      }
+
+      // Try to find the product by slug
+      else {
+        $product = $products->find($id);
+      }
+
+      // Define the result as the product if it exists
       if ($product) {
         $result = [
           "success" => true,
           "product" => [
+            "id" => $product->id(),
+            "permalink" => $product->permalink(),
             "name" => $product->title()->toString(),
+            "author" => $product->author()->toString(),
           ]
         ];
-      } else {
+      }
+
+      // Define the result as not found if the product doesn't exist
+      else {
         $result = [
           "success" => false,
         ];
       }
 
+      // Return the result as JSON
       return Response::json($result, null, true);
     }
   ],
