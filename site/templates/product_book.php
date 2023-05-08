85b71dcde727ef21cb128697bf9780a2080e9088
apademide <contact@apademide.ch>
2023-05-08 13:58:35 +0200

    imp: include price and remaining data

--- site/templates/product_book.php
+++ site/templates/product_book.php
@@ -10,7 +10,39 @@ $data = [
   "name" => $page->title()->toString(),
   "author" => $page->author()->toString(),
   "publisher" => $page->publisher()->toString(),
+  "href" => $page->url(),
+  "remaining" => (function ($page) {
+    $remaining = $page->stock()->toInt();
 
+    if ($remaining <= 0) {
+      return "none";
+    } else if ($remaining === 1) {
+      return "one";
+    } else if ($remaining <= 3) {
+      return "few";
+    } else if ($remaining <= 5) {
+      return "many";
+    } else {
+      return "all";
+    }
+  })($page),
+  "price" => (function ($page) {
+
+    $price = $page->price()->toFloat();
+    $currency = "CHF";
+
+    if ($page->auto_kerns()->toBool() === false) {
+      $kerns = $page->kerns()->toFloat();
+    } else {
+      $kerns = (int)($price * 100);
+    }
+
+    return [
+      "amount" => $price,
+      "currency" => $currency,
+      "kerns" => $kerns
+    ];
+  })($page)
 ];
 
 // JSON string
