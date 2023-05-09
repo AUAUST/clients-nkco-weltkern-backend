6296533ac5bf2e500ba9c1b1aa8d11c05fd05d76
apademide <contact@apademide.ch>
2023-05-09 14:58:38 +0200

    debug: disable debug code

--- site/templates/page_home.php
+++ site/templates/page_home.php
@@ -39,16 +39,16 @@ $response->send();
 
 <?php
 
-$font = Font::load(__DIR__ . '/TWKLausanne-500.otf');
-$font->parse(); // for getFontWeight() to work this call must be done first!
-echo $font->getFontName() . '<br>';
-echo $font->getFontSubfamily() . '<br>';
-echo $font->getFontSubfamilyID() . '<br>';
-echo $font->getFontFullName() . '<br>';
-echo $font->getFontVersion() . '<br>';
-echo $font->getFontWeight() . '<br>';
-echo $font->getFontPostscriptName() . '<br>';
-dump($font);
-$font->close();
+// $font = Font::load(__DIR__ . '/TWKLausanne-500.otf');
+// $font->parse(); // for getFontWeight() to work this call must be done first!
+// echo $font->getFontName() . '<br>';
+// echo $font->getFontSubfamily() . '<br>';
+// echo $font->getFontSubfamilyID() . '<br>';
+// echo $font->getFontFullName() . '<br>';
+// echo $font->getFontVersion() . '<br>';
+// echo $font->getFontWeight() . '<br>';
+// echo $font->getFontPostscriptName() . '<br>';
+// dump($font);
+// $font->close();
 
 ?>
