c17eeab439d190a9a30b58cdbc223593f67b99d6
apademide <contact@apademide.ch>
2023-05-09 10:34:12 +0200

    imp: /home returns basic data

--- site/templates/page_home.php
+++ site/templates/page_home.php
@@ -1,1 +1,29 @@
-I'm home
+<?php
+
+use Kirby\Cms\Response;
+use Kirby\Data\Json;
+
+// Product's data
+$data = [
+  "title" => $page->title()->toString(),
+  "shortTitle" => $page->short_title()->toString(),
+];
+
+// JSON string
+$body = Json::encode($data);
+
+// Response object
+$response = Response::json(
+  $data,
+  null,
+  true
+);
+
+?>
+
+<?=
+
+// Send the response
+$response->send();
+
+?>
