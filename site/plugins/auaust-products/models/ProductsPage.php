1643ddabab5c32653b549cceced0e10e2a65e5d3
apademide <contact@apademide.ch>
2023-05-16 15:48:09 +0200

    feat: fetch product quantity from Weltkern

--- site/plugins/auaust-products/models/ProductsPage.php
+++ site/plugins/auaust-products/models/ProductsPage.php
@@ -1,11 +1,29 @@
 <?php
 
 use Kirby\Cms\Page;
+use Kirby\Http\Remote;
 
 class ProductsPage extends Page
 {
   public function cover()
   {
-    return "The page model is working!";
+    return "The page model is working and Weltkern has {$this->getWeltkernProductsQuantity()} products.";
+  }
+
+  private function getWeltkernProductsQuantity()
+  {
+    $response = Remote::get("https://api.weltkern.com/wp-json/custom-routes/v1/products/total");
+
+    if ($response->code() !== 200) {
+      return 0;
+    }
+
+    $quantity = intval($response->content());
+
+    if ($quantity < 0) {
+      return 0;
+    }
+
+    return $quantity;
   }
 }
