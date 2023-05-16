b69e15e55a421659da0d0cd2ad132adb1b2492ce
apademide <contact@apademide.ch>
2023-05-16 10:21:59 +0200

    snap: fetchimage, untested

---
+++ site/plugins/auaust-fetchimage/index.php
@@ -0,0 +1,56 @@
+<?php
+
+use Kirby\Cms\App as Kirby;
+use Kirby\Http\Remote;
+use Kirby\Filesystem\Dir;
+use Kirby\Filesystem\F;
+
+
+Kirby::plugin('auaust/fetchimage', [
+  'pageMethods' => [
+    'fetchImage' => function (
+      string $url,
+      string|null $fileName = null,
+      $template = null,
+    ) {
+
+
+      $response = Remote::get($url);
+
+      if ($response->code() !== 200) {
+        throw new Exception('Could not fetch image: Code ' . $response->code());
+      }
+
+      $fileName ??= basename($url);
+      $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+
+      $cacheRootPath = kirby()->root('cache');
+
+      $temppath = sprintf(
+        '%s/%s.%s',
+        $cacheRootPath,
+        bin2hex(random_bytes(5)),
+        $fileExtension
+      );
+
+      if (!Dir::exists($cacheRootPath)) {
+        Dir::make($cacheRootPath);
+      }
+
+      file_put_contents($temppath, $response->content());
+
+      if ($file = $this->file($fileName)) {
+        $file->replace($temppath);
+      } else {
+        $file = $this->createFile([
+          'parent' => $this,
+          'source' => $temppath,
+          'filename' => $fileName,
+          'template' => $template
+        ]);
+      }
+
+      // F::remove($temppath);
+    }
+  ],
+]);
