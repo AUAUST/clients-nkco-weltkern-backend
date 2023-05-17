4200c6c12885ab0ba351cc30bc7396100acf551b
apademide <contact@apademide.ch>
2023-05-17 17:05:14 +0200

    debug: created a monster

--- site/templates/products.php
+++ site/templates/products.php
@@ -1,19 +1,69 @@
-<?php
+<pre><?php
 
-use auaust\products\WK1;
-use Kirby\Cms\Url;
+      use auaust\products\WK1;
 
-// $products = WK1::products();
-// $quantity = WK1::productsQuantity();
+      $products = WK1::products();
+      $quantity = WK1::productsQuantity();
 
-// foreach ($products as $product) {
-//   echo $product['name'] . '<br>';
-// }
+      foreach ($products as $index => $product) {
+        // pad left index (1 -> 0001)
+        $index = str_pad($index + 1, 3, '0', STR_PAD_LEFT);
+        // {{ index }} : {{ product.name }}
+        echo "============================== " . $index . " ==============================<br>";
+        echo $product['name'] . '<br>';
+        echo "-----------------------------------------------------------------<br>";
+        // print all product properties
+        foreach ($product as $key => $value) {
+          // nest if array
+          if (is_array($value)) {
+            echo $key . ':<br>';
+            foreach ($value as $key => $value) {
+              // nest if array
+              if (is_array($value)) {
+                echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ':<br>';
+                foreach ($value as $key => $value) {
+                  // nest if array
+                  if (is_array($value)) {
+                    echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ':<br>';
+                    foreach ($value as $key => $value) {
+                      // nest if array
+                      if (is_array($value)) {
+                        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ':<br>';
+                        foreach ($value as $key => $value) {
+                          // nest if array
+                          if (is_array($value)) {
+                            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ':<br>';
+                            foreach ($value as $key => $value) {
+                              // nest if array
+                              if (is_array($value)) {
+                                echo "too deep<br>";
+                                continue;
+                              }
+                              echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ': ' . $value . '<br>';
+                            }
+                            continue;
+                          }
 
+                          echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ': ' . $value . '<br>';
+                        }
+                        continue;
+                      }
 
-dump([
-  "url" => $url = "https://www.auaust.com/products",
-  "path" => Url::path($url, false),
-  "reUrl" => $url = "/products",
-  "rePath" => Url::path($url, false),
-]);
+                      echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ': ' . $value . '<br>';
+                    }
+                    continue;
+                  }
+
+                  echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ': ' . $value . '<br>';
+                }
+                continue;
+              }
+
+              echo '&nbsp;&nbsp;&nbsp;&nbsp;' . $key . ': ' . $value . '<br>';
+            }
+            continue;
+          }
+          echo $key . ': ' . $value . '<br>';
+        }
+      }
+      ?></pre>
