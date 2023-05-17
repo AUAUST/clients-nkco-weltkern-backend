8d95158f43953e21c81961444a67abb6eed32f1f
apademide <contact@apademide.ch>
2023-05-17 16:53:14 +0200

    debug: check function works

--- site/templates/products.php
+++ site/templates/products.php
@@ -1,10 +1,19 @@
 <?php
 
 use auaust\products\WK1;
+use Kirby\Cms\Url;
 
-$products = WK1::products();
-$quantity = WK1::productsQuantity();
+// $products = WK1::products();
+// $quantity = WK1::productsQuantity();
 
-foreach ($products as $product) {
-  echo $product['name'] . '<br>';
-}
+// foreach ($products as $product) {
+//   echo $product['name'] . '<br>';
+// }
+
+
+dump([
+  "url" => $url = "https://www.auaust.com/products",
+  "path" => Url::path($url, false),
+  "reUrl" => $url = "/products",
+  "rePath" => Url::path($url, false),
+]);
