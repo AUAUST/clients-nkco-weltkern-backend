4e148726a1d03ebffd4a38d86eccc44bc4939505
apademide <contact@apademide.ch>
2023-05-22 19:21:12 +0200

    style: disabled code as PHP comments

--- site/templates/products.php
+++ site/templates/products.php
@@ -93,57 +93,48 @@ function titleLine(string $title = "", int $columnWidth = 101)
     </section>
   <?php endforeach; ?>
 </div>
-<!--
-       {
+<?php
+// ALL PROPS
+// id, name, slug, description, short_description
+// featured_image, gallery_image, featured, typeface, typefaces
+// price, price_welt, in_stock, weight, length
+// width, height, downloadable, categories, tags
+// brands, average_rating, review_count, quantity, quantite
+// back_order_qty, only_welt_point, multiplier, new, rare
+// made_by_weltanschauung, staff_pick, weltclub_exclu, download, in_use
+// choice_product, block_text, header, licences, font_feature
+// poids, estimation_de_livraison, estimation_back_order, gift_wrap, frais_livraisons
+// header_color, welt_price, options, estimation_date_backorder, content_story
+// display_story, variant, backorder_check, categorie_multiplier, 0
+// 1, 2, 3, 4, font_face
+// points, currency, colors
 
-        // pad left index (1 -> 0001)
-        $index = str_pad($index + 1, 3, '0', STR_PAD_LEFT);
-        // {{ index }} : {{ product.name }}
-        echo "============================== " . $index . " ==============================<br>";
-        echo $product['name'] . '<br>';
-        echo "-----------------------------------------------------------------<br>";
-        // ALL PROPS
-        // id, name, slug, description, short_description
-        // featured_image, gallery_image, featured, typeface, typefaces
-        // price, price_welt, in_stock, weight, length
-        // width, height, downloadable, categories, tags
-        // brands, average_rating, review_count, quantity, quantite
-        // back_order_qty, only_welt_point, multiplier, new, rare
-        // made_by_weltanschauung, staff_pick, weltclub_exclu, download, in_use
-        // choice_product, block_text, header, licences, font_feature
-        // poids, estimation_de_livraison, estimation_back_order, gift_wrap, frais_livraisons
-        // header_color, welt_price, options, estimation_date_backorder, content_story
-        // display_story, variant, backorder_check, categorie_multiplier, 0
-        // 1, 2, 3, 4, font_face
-        // points, currency, colors
+// USEFUL PROPS
+// name, slug, short_description
+// featured_image: {
+//  url: false|url
+//  id: number
+// }
 
-        // USEFUL PROPS
-        // name, slug, short_description
-        // featured_image: {
-        //  url: false|url
-        //  id: number
-        // }
-
-        // , gallery_image, typefaces
-        // price, price_welt, in_stock, weight, length
-        // width, height, downloadable, categories, tags
-        // brands, average_rating, review_count, quantity, quantite
-        // back_order_qty, only_welt_point, multiplier, new, rare
-        // made_by_weltanschauung, staff_pick, weltclub_exclu, download, in_use
-        // choice_product, block_text, header, licences, font_feature
-        // poids, estimation_de_livraison, estimation_back_order, gift_wrap, frais_livraisons
-        // header_color, welt_price, options, estimation_date_backorder, content_story
-        // display_story, backorder_check, categorie_multiplier, 0
-        // 1, 2, 3, 4
-        // points, currency, colors
-        echo "-----------------------------------------------------------------<br>";
-        echo "cover<div style='display:flex;gap:10px'>";
-        echo "<img src='" . $imagesUrls[$product['featured_image']['id']] . "' alt='" . $product['name'] . "' height='300' loading='lazy'><div>";
-        foreach ($product['gallery_image'] as $index => $image) {
-          if ($index > 0)
-            echo "<img src='" . $imagesUrls[$image['id']] . "' alt='" . $product['name'] . "' height='200' loading='lazy'>";
-        }
-        echo "</div></div>";
-        echo "<br><br><br><br><br>";
-      }
-      ?></pre> -->
+// , gallery_image, typefaces
+// price, price_welt, in_stock, weight, length
+// width, height, downloadable, categories, tags
+// brands, average_rating, review_count, quantity, quantite
+// back_order_qty, only_welt_point, multiplier, new, rare
+// made_by_weltanschauung, staff_pick, weltclub_exclu, download, in_use
+// choice_product, block_text, header, licences, font_feature
+// poids, estimation_de_livraison, estimation_back_order, gift_wrap, frais_livraisons
+// header_color, welt_price, options, estimation_date_backorder, content_story
+// display_story, backorder_check, categorie_multiplier, 0
+// 1, 2, 3, 4
+// points, currency, colors
+// echo "-----------------------------------------------------------------<br>";
+// echo "cover<div style='display:flex;gap:10px'>";
+// echo "<img src='" . $imagesUrls[$product['featured_image']['id']] . "' alt='" . $product['name'] . "' height='300' loading='lazy'><div>";
+// foreach ($product['gallery_image'] as $index => $image) {
+//   if ($index > 0)
+//     echo "<img src='" . $imagesUrls[$image['id']] . "' alt='" . $product['name'] . "' height='200' loading='lazy'>";
+// }
+// echo "</div></div>";
+// echo "<br><br><br><br><br>";
+?>
