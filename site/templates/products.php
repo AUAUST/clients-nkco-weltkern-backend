0aa77a62a659759c768b147e65ac5400b5c36d73
apademide <contact@apademide.ch>
2023-05-22 15:32:47 +0200

    debug: start creating ref of useful props

--- site/templates/products.php
+++ site/templates/products.php
@@ -3,9 +3,9 @@
 use Kirby\Toolkit\Str;
 use auaust\products\WK1;
 
-dump(WK1::products()[0]);
-echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
-dump(WK1::products()[10]);
+// dump(WK1::products()[0]);
+// echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+dump(WK1::products()[9]);
 echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
 ?>
 <pre><?php
@@ -21,6 +21,7 @@ echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><b
         echo "============================== " . $index . " ==============================<br>";
         echo $product['name'] . '<br>';
         echo "-----------------------------------------------------------------<br>";
+        // ALL PROPS
         // id, name, slug, description, short_description
         // featured_image, gallery_image, featured, typeface, typefaces
         // price, price_welt, in_stock, weight, length
@@ -34,14 +35,34 @@ echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><b
         // display_story, variant, backorder_check, categorie_multiplier, 0
         // 1, 2, 3, 4, font_face
         // points, currency, colors
+
+        // USEFUL PROPS
+        // name, slug, short_description
+        // featured_image: {
+        //  url: false|url
+        //  id: number
+        // }
+
+        // , gallery_image, typefaces
+        // price, price_welt, in_stock, weight, length
+        // width, height, downloadable, categories, tags
+        // brands, average_rating, review_count, quantity, quantite
+        // back_order_qty, only_welt_point, multiplier, new, rare
+        // made_by_weltanschauung, staff_pick, weltclub_exclu, download, in_use
+        // choice_product, block_text, header, licences, font_feature
+        // poids, estimation_de_livraison, estimation_back_order, gift_wrap, frais_livraisons
+        // header_color, welt_price, options, estimation_date_backorder, content_story
+        // display_story, backorder_check, categorie_multiplier, 0
+        // 1, 2, 3, 4
+        // points, currency, colors
         echo "slug<br>(WP) " . $product['slug'] . '<br>';
-        echo "(K0) " . Str::slug(Str::unhtml($product['name'])) . '<br>';
+        echo "(K3) " . Str::slug(Str::unhtml($product['name'])) . '<br>';
         echo "-----------------------------------------------------------------<br>";
         echo "type<br>  " . $product['categories'][0]['slug'] . '<br>';
         echo "-----------------------------------------------------------------<br>";
         echo "description<br>  " . str_replace(PHP_EOL, "<br>  ", Str::unhtml($product['short_description'])) . '<br>';
         echo "-----------------------------------------------------------------<br>";
-        echo "font_face<br>  " . json_encode($product['font_face']) . '<br>';
+        echo "kkkkk<br>  " . json_encode($product['featured_image']) . '<br>';
         echo "<br><br><br><br><br>";
       }
       ?></pre>
