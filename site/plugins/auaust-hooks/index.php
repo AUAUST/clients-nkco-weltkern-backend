cafa0dbcbcf82a4355607b7b5dd055a1358388a9
apademide <contact@apademide.ch>
2023-05-23 15:36:47 +0200

    feat: set initial fields after designer created

--- site/plugins/auaust-hooks/index.php
+++ site/plugins/auaust-hooks/index.php
@@ -4,12 +4,31 @@ use Kirby\Cms\App as Kirby;
 
 Kirby::plugin("auaust/hooks", [
   'hooks' => [
-    'page.create:after' => function ($event) {
-      $data = dump($event, false);
+    'page.create:after' => function ($page) {
 
-      $file = kirby()->root('content') . '/hooks.txt';
+      // We only want to run this hook when a designer is added
+      if ($page->intendedTemplate()->name() !== "designer") {
+        return;
+      }
 
-      file_put_contents($file, $data);
+      // Get the designer's name
+      $fullname = $page->title();
+
+      // Split the name into first and last name.
+      // "Anthony Eric Monnier" => ["Anthony", "Eric", "Monnier"]
+      $nameparts = explode(" ", $fullname);
+      // "Monnier"
+      $lastname = array_pop($nameparts);
+      // "Anthony Eric"
+      $firstname = implode(" ", $nameparts);
+
+      // Update the page's "name" field (object with firstname and lastname)
+      $page->update([
+        "name" => [
+          "firstname" => $firstname,
+          "lastname" => $lastname
+        ]
+      ]);
     }
   ]
 ]);
