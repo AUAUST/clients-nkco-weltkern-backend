a460ef6b1b02bb9a7da14b32cafe6ad0d174676f
apademide <contact@apademide.ch>
2023-05-23 15:26:33 +0200

    debug: show hook data

--- site/plugins/auaust-hooks/index.php
+++ site/plugins/auaust-hooks/index.php
@@ -2,4 +2,14 @@
 
 use Kirby\Cms\App as Kirby;
 
-Kirby::plugin("auaust/hooks", []);
+Kirby::plugin("auaust/hooks", [
+  'hooks' => [
+    'page.create:after' => function ($event) {
+      $data = dump($event, false);
+
+      $file = kirby()->root('content') . '/hooks.txt';
+
+      file_put_contents($file, $data);
+    }
+  ]
+]);
