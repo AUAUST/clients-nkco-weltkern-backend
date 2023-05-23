8347ac56c725c8136e507953861b128c3e55cd4b
apademide <contact@apademide.ch>
2023-05-23 17:17:41 +0200

    comments: explain code

--- site/plugins/auaust-designers/models/DesignerPage.php
+++ site/plugins/auaust-designers/models/DesignerPage.php
@@ -8,31 +8,37 @@ class DesignerPage extends Page
 {
   public static function create(array $props): Page
   {
-    // Split the name into first and last name.
+
+    // Try to guess the designer's full name from the page's title at creation
     // "Anthony Eric Monnier" => ["Anthony", "Eric", "Monnier"]
     $nameparts = explode(" ", $props['content']['title']);
+
     // "Monnier"
     $lastname = array_pop($nameparts);
-    // "Anthony Eric"
-    $firstname = implode(" ", $nameparts);
 
-    // Update the page's "name" field (object with firstname and lastname)
+    // "Anthony Eric"
+    $firstname = implode("\u{00a0}", $nameparts);
+
+    // Update the page's "name" field default value
     $props['content']['name'] = [
       "firstname" => $firstname,
       "lastname" => $lastname
     ];
 
+    // Create the page with updated props
     return parent::create($props);
   }
 
   public function title()
   {
+    // Get the designer's name from the "name" field
     $name = $this->content()->get("name")->toObject();
 
     return new Field(
       $this,
       "title",
-      $name->firstname() . " " . $name->lastname()
+      // Join the first and last name with a non-breaking space
+      $name->firstname() . "\u{00a0}" . $name->lastname()
     );
   }
 }
