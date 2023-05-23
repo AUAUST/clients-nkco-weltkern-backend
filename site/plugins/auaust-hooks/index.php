52955e464543b0d041fe9e83fd8232f30acc8bca
apademide <contact@apademide.ch>
2023-05-23 15:40:09 +0200

    style: switch to switch rather than if

--- site/plugins/auaust-hooks/index.php
+++ site/plugins/auaust-hooks/index.php
@@ -6,29 +6,32 @@ Kirby::plugin("auaust/hooks", [
   'hooks' => [
     'page.create:after' => function ($page) {
 
-      // We only want to run this hook when a designer is added
-      if ($page->intendedTemplate()->name() !== "designer") {
-        return;
+      // switch by template using $page->intendedTemplate()->name()
+      switch ($page->intendedTemplate()->name()) {
+
+          /*
+        After a new designer page is created
+          */
+        case "designer":
+          // Get the designer's name
+          $fullname = $page->title();
+
+          // Split the name into first and last name.
+          // "Anthony Eric Monnier" => ["Anthony", "Eric", "Monnier"]
+          $nameparts = explode(" ", $fullname);
+          // "Monnier"
+          $lastname = array_pop($nameparts);
+          // "Anthony Eric"
+          $firstname = implode(" ", $nameparts);
+
+          // Update the page's "name" field (object with firstname and lastname)
+          $page->update([
+            "name" => [
+              "firstname" => $firstname,
+              "lastname" => $lastname
+            ]
+          ]);
       }
-
-      // Get the designer's name
-      $fullname = $page->title();
-
-      // Split the name into first and last name.
-      // "Anthony Eric Monnier" => ["Anthony", "Eric", "Monnier"]
-      $nameparts = explode(" ", $fullname);
-      // "Monnier"
-      $lastname = array_pop($nameparts);
-      // "Anthony Eric"
-      $firstname = implode(" ", $nameparts);
-
-      // Update the page's "name" field (object with firstname and lastname)
-      $page->update([
-        "name" => [
-          "firstname" => $firstname,
-          "lastname" => $lastname
-        ]
-      ]);
     }
   ]
 ]);
