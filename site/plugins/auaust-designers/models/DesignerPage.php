455dd60e7e946a0ddb5700c278410e688702d7b2
apademide <contact@apademide.ch>
2023-05-24 14:18:04 +0200

    snap: revert the whole slug alert thing

--- site/plugins/auaust-designers/models/DesignerPage.php
+++ site/plugins/auaust-designers/models/DesignerPage.php
@@ -25,6 +25,12 @@ class DesignerPage extends Page
     ];
   }
 
+  private static function makeName(string $firstname, string $lastname): string
+  {
+    // Join the first and last name with a non-breaking space and trim the result
+    return trim($firstname . "\u{00a0}" . $lastname, "\u{00a0} ");
+  }
+
   public static function create(array $props): Page
   {
     // Update the page's "name" field default value
@@ -36,16 +42,11 @@ class DesignerPage extends Page
     return parent::create($props);
   }
 
-  private static function makeName(string $firstname, string $lastname): string
-  {
-    // Join the first and last name with a non-breaking space and trim the result
-    return trim($firstname . "\u{00a0}" . $lastname, "\u{00a0} ");
-  }
-
   public function title()
   {
     // Get the designer's name from the "name" field
     $name = $this->content()->get("names")->toObject();
+
 
     $name = self::makeName(
       $name->firstname(),
@@ -59,51 +60,21 @@ class DesignerPage extends Page
     );
   }
 
-  public function changeTitle(string $title, ?string $languageCode = null)
-  {
-    // Replace all kinds of spaces with a single space
-    $title = preg_replace("/\s+/", ' ', $title);
+  // public function changeTitle(string $title, ?string $languageCode = null)
+  // {
+  //   // Replace all kinds of spaces with a single space
+  //   $title = preg_replace("/\s+/", ' ', $title);
 
-    // Trim the title
-    $title = trim($title);
+  //   // Trim the title
+  //   $title = trim($title);
 
-    // The title is determined by the "name" field, so we need to update it rather than the title field
-    // The title field is removed to prevent confusion (mismatch between the title field and the actual title)
-    $page = $this->update([
-      "title" => null,
-      "names" => self::splitName($title)
-    ]);
+  //   // The title is determined by the "name" field, so we need to update it rather than the title field
+  //   // The title field is removed to prevent confusion (mismatch between the title field and the actual title)
+  //   $page = $this->update([
+  //     "title" => null,
+  //     "names" => self::splitName($title)
+  //   ]);
 
-    return $page->checkSlug();
-  }
-
-  public function update(array $input = null, string $languageCode = null, bool $validate = false)
-  {
-
-    // If the names are being updated, check if the slug needs to be updated too
-    if (isset($input["names"])) {
-    }
-
-    return parent::update($input, $languageCode, $validate);
-  }
-
-  private function checkSlug(): static
-  {
-    // If the new title implies a new slug, show a warning in the panel
-    $expectedSlug = Str::slug($this->title());
-
-    // If the slug is the same as the expected slug, there's no mismatch
-    if ($this->slug() === $expectedSlug) {
-      return $this->update([
-        "hasMismatchedSlug" => "false",
-        "ignoreMismatchedSlug" => "false"
-      ]);
-    }
-
-    // Otherwise, show a warning in the panel
-    return $this->update([
-      "hasMismatchedSlug" => "true",
-      "ignoreMismatchedSlug" => "false"
-    ]);
-  }
+  //   return $page;
+  // }
 }
