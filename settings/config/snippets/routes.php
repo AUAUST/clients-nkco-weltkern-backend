c3d31161a7a08e01f55e572b49837f8ea183314c
apademide <contact@apademide.ch>
2023-05-31 11:25:19 +0200

    imp: extract "details" key

--- settings/config/snippets/routes.php
+++ settings/config/snippets/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Kirby\Cms\Response;
+use Kirby\Toolkit\Str;
 use auaust\products\WK1;
 
 return [
@@ -83,31 +84,65 @@ return [
         }
 
         $returnString .= 'name: ' . $product['name'] . '<br>';
-        $returnString .= 'slug: ' . $product['slug'] . '<br>';
-        $returnString .= 'id: ' . $product['id'] . '<br>';
-        $returnString .= 'short_description: ' . $product['short_description'] . '<br>';
-        $returnString .= 'gallery_image: ' . json_encode($product['gallery_image']) . '<br>';
-        $returnString .= 'price: ' . $product['price'] . '<br>';
-        $returnString .= 'weight: ' . $product['weight'] . '<br>';
-        $returnString .= 'categories: ' . json_encode($product['categories']) . '<br>';
-        $returnString .= 'tags: ' . json_encode($product['tags']) . '<br><br>';
-        $returnString .= 'header: ' . json_encode($product['header'][0]['header']) . '<br><br><br>';
+        // $returnString .= 'slug: ' . $product['slug'] . '<br>';
+        // $returnString .= 'id: ' . $product['id'] . '<br>';
+        // $returnString .= 'short_description: ' . $product['short_description'] . '<br>';
+        // $returnString .= 'gallery_image: ' . json_encode($product['gallery_image']) . '<br>';
+        // $returnString .= 'price: ' . $product['price'] . '<br>';
+        // $returnString .= 'weight: ' . $product['weight'] . '<br>';
+        // $returnString .= 'categories: ' . json_encode($product['categories']) . '<br>';
+        // $returnString .= 'tags: ' . json_encode($product['tags']) . '<br><br>';
+        // $returnString .= 'header keys: ' . json_encode(array_keys($product['header'][0]['header'])) . '<br><br><br>';
 
-        $author = $product['header'][0]['header']['author_information']['author'];
-        $returnString .= 'author_name: ' . $author['name'] . '<br>';
-        $returnString .= 'author_id: ' . $author['term_id'] . '<br>';
+        foreach ($product['header'][0]['header'] as $key => $value) {
 
-        $returnString .= 'poids: ' . json_encode($product['poids']) . '<br>';
-        $returnString .= 'header_color: ' . $product['header_color'] . '<br>';
-        $returnString .= 'options: ' . json_encode($product['options']) . '<br>';
-        $returnString .= 'ISBN: ' . (function () use ($product) {
-          foreach ($product['header'][0]['header']['block_option'] as $option) {
-            if ($option['option'] === 'ISBN') {
-              return $option['value'];
-            }
+          if ($key === 'block_option') {
+            $returnString .= 'block_option: ' . json_encode($value) . '<br><br>';
           }
-          return 'NO ISBN';
-        })() . '<br>';
+
+          // $returnString .= $key . ': ';
+
+          // if ($key === 'block_option') {
+
+          //   $returnString .= '<br>';
+
+          //   foreach ($value as $option) {
+          //     $returnString .= '— ' . $option['option'] . ': ' . $option['value'] . '<br>';
+          //   }
+          // }
+
+          // if ($key === 'text_grand') {
+          //   $returnString .= '<br>';
+          //   $returnString .= '— ' . $value . '<br>';
+
+          //   $returnString .= $product['short_description'] . '<br>';
+          // }
+
+          // if ($key === 'block_option_center') {
+          //   $returnString .= '<br>';
+          //   $returnString .= json_encode($value) . '<br>';
+          // }
+
+          //  . Str::unhtml(json_encode($value)) . '<br><br>';
+        }
+
+        // $returnString .= 'header: ' . json_encode($product['header'][0]['header']) . '<br><br><br>';
+
+        // $author = $product['header'][0]['header']['author_information']['author'];
+        // $returnString .= 'author_name: ' . $author['name'] . '<br>';
+        // $returnString .= 'author_id: ' . $author['term_id'] . '<br>';
+
+        // $returnString .= 'poids: ' . json_encode($product['poids']) . '<br>';
+        // $returnString .= 'header_color: ' . $product['header_color'] . '<br>';
+        // $returnString .= 'options: ' . json_encode($product['options']) . '<br>';
+        // $returnString .= 'ISBN: ' . (function () use ($product) {
+        //   foreach ($product['header'][0]['header']['block_option'] as $option) {
+        //     if ($option['option'] === 'ISBN') {
+        //       return $option['value'];
+        //     }
+        //   }
+        //   return 'NO ISBN';
+        // })() . '<br>';
         $returnString .= '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
 
         // oldWeltkern
