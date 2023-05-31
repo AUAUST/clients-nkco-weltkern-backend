9698a4bbe5bd7a436eef5d2be1f8a716f3dd52fb
apademide <contact@apademide.ch>
2023-05-31 14:57:41 +0200

    style: remove placeholder, useless method

--- site/plugins/auaust-products/models/ProductsPage.php
+++ site/plugins/auaust-products/models/ProductsPage.php
@@ -6,11 +6,6 @@ use auaust\products\WK1;
 
 class ProductsPage extends Page
 {
-  public function cover()
-  {
-    // $quantity = WK1::productsQuantity();
-    return WK1::products();
-  }
   public function sum()
   {
     return WK1::productsQuantity();
