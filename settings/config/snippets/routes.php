0fa534e7ad68c997c50565709e1b7a8c1686a76a
apademide <contact@apademide.ch>
2023-05-31 11:08:35 +0200

    imp: extract all data

--- settings/config/snippets/routes.php
+++ settings/config/snippets/routes.php
@@ -129,38 +129,66 @@ return [
         //         name
         //         id
 
+        $productsPage->createChild([
+          'num' => $index + 1,
+          'slug' => $product['name'],
+          'template' => 'product_book',
+          'content' => [
+            'oldWeltkern' => [
+              'title' => $product['name'],
 
-        // $productsPage->createChild([
-        //   'num' => $index + 1,
-        //   'slug' => $product['name'],
-        //   'template' => 'product_book',
-        //   'content' => [
-        //     'oldWeltkern' => [
-        //       'slug' => $product['slug'],
-        //       'id' => $product['id'],
-        //       'description' => $product['short_description'],
-        //       'gallery' => (array_map(
-        //         function ($image) {
-        //           return [
-        //             'url' => $image['url'],
-        //             'id' => $image['id'],
-        //           ];
-        //         },
-        //         $product['gallery_image']
-        //       )),
-        //       'tags' => (array_map(
-        //         function ($tag) {
-        //           return [
-        //             'name' => $tag['name'],
-        //             'id' => $tag['id'],
-        //           ];
-        //         },
-        //         $product['tags']
-        //       )
-        //       )
-        //     ]
-        //   ]
-        // ]);
+              'slug' => $product['slug'],
+
+              'id' => $product['id'],
+
+              'isbn' => (function () use ($product) {
+                foreach ($product['header'][0]['header']['block_option'] as $option) {
+                  if ($option['option'] === 'ISBN') {
+                    return $option['value'];
+                  }
+                }
+                return 'NO ISBN';
+              })(),
+
+              'price' => $product['price'],
+
+              'weight' => $product['weight'],
+
+              'author' => (function () use ($product) {
+                $author = $product['header'][0]['header']['author_information']['author'];
+                return [
+                  'name' => $author['name'],
+                  'id' => $author['term_id'],
+                ];
+              })(),
+
+              'description' => $product['short_description'],
+
+              'details' => null,
+
+              'gallery' => (array_map(
+                function ($image) {
+                  return [
+                    'url' => $image['url'],
+                    'id' => $image['id'],
+                  ];
+                },
+                $product['gallery_image']
+              )),
+
+              'tags' => (array_map(
+                function ($tag) {
+                  return [
+                    'name' => $tag['name'],
+                    'id' => $tag['id'],
+                  ];
+                },
+                $product['tags']
+              )
+              )
+            ]
+          ]
+        ]);
 
 
         // $newProducts[] = [
