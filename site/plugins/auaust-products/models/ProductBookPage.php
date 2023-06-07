a50d1356c7830b8065c3df686ee4a924c1171f22
apademide <contact@apademide.ch>
2023-06-07 12:07:27 +0200

    feat: $page→titleWithSeparator()

--- site/plugins/auaust-products/models/ProductBookPage.php
+++ site/plugins/auaust-products/models/ProductBookPage.php
@@ -37,11 +37,35 @@ class ProductBookPage extends Page
 
   public function title()
   {
+    return $this->titleWithSeparator();
+  }
+
+  public function titleWithSeparator(string|null $separator = null)
+  {
+    // Use the parameter separator if provided, otherwise use the page's multilineTitleSeparator field
+    $separator =
+      $separator
+      ?? $this->content()->get("multilineTitleSeparator")->toString()
+      ?? 'space';
+
     // Get the title from the multilineTitle field
     $title = $this->content()->get("multilineTitle")->toString();
 
+    // Map the separator type to a separator string
+    $separators = [
+      'space' => ' ',
+      'dash' => ' – '
+    ];
+
+    // Fall back to space by default in case an invalid separator type is provided somehow
+    if (array_key_exists($separator, $separators)) {
+      $separator = $separators[$separator];
+    } else {
+      $separator = $separators['space'];
+    }
+
     // Replace line breaks by em spaces
-    $title = preg_replace('/\R/u', ' ', $title);
+    $title = preg_replace('/\R/u', $separator, $title);
 
     return new Field(
       $this,
