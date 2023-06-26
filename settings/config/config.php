a3038e71340ada6e3542cf26da2cdab5f404c5cd
apademide <contact@apademide.ch>
2023-06-26 16:11:19 +0200

    fix: wait for secret function to be available

--- settings/config/config.php
+++ settings/config/config.php
@@ -12,8 +12,13 @@ return [
 
   'routes' => require_once __DIR__ . '/routes/index.php',
 
-  'auaust.algolia' => [
-    'algoliaAppId'    => secret('ALGOLIA_APP_ID'),
-    'algoliaAdminKey' => secret('ALGOLIA_ADMIN_KEY')
-  ]
+  // Using 'ready' otherwise 'secret' isn't yet available
+  'ready' => function () {
+    return [
+      'auaust.algolia' => [
+        'algoliaAppId'    => secret('ALGOLIA_APP_ID'),
+        'algoliaAdminKey' => secret('ALGOLIA_ADMIN_KEY')
+      ]
+    ];
+  }
 ];
