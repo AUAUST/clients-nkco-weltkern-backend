c61f9d4e8b978381b83739eb1d85e3a6a0b1d596
apademide <contact@apademide.ch>
2023-06-27 13:33:53 +0200

    snap: basic functions to connect to algolia

--- site/plugins/auaust-algolia/classes/Algolia.php
+++ site/plugins/auaust-algolia/classes/Algolia.php
@@ -2,10 +2,46 @@
 
 namespace AUAUST\Algolia;
 
+use Algolia\AlgoliaSearch\Config\SearchConfig;
+use Algolia\AlgoliaSearch\SearchClient;
+
 class Algolia
 {
-  public static function productsQuantity()
+  private static SearchClient|null $client;
+  private static $indices = [];
+
+  public static function initIndex(string $indexName)
   {
-    return 100;
+    if (
+      !($indexConfig = option("auaust.algolia.indices.{$indexName}"))
+    ) {
+      throw new \Exception("Index '{$indexName}' not found");
+    }
+
+    if (is_array($indexConfig['settings'])) {
+      $config['settings'] = $indexConfig['settings'];
+    }
+
+    $index = self::client()->initIndex($indexName);
+
+    return $index;
+  }
+
+  public static function client()
+  {
+    if (isset(self::$client)) {
+      return self::$client;
+    }
+
+    $config = [
+      'appId'  => option('auaust.algolia.algoliaAppId'),
+      'apiKey' => option('auaust.algolia.algoliaApiKey'),
+    ];
+
+    self::$client = SearchClient::createWithConfig(
+      new SearchConfig($config)
+    );
+
+    return self::$client;
   }
 }
