c984a2dc4484d599a1ca5a2c5149fc5bc24cf067
apademide <contact@apademide.ch>
2023-06-28 11:27:31 +0200

    snap: move migration function to new route

--- settings/config/routes/weltkern-migration.php
+++ settings/config/routes/weltkern-migration.php
@@ -1,6 +1,8 @@
 <?php
 
 use Kirby\Cms\Response;
+use Kirby\Toolkit\Str;
+
 use AUAUST\products\WK1;
 
 return [
@@ -8,7 +10,126 @@ return [
   [
     'pattern' => 'sync-weltkern',
     'language' => '*',
-    'action' => function ($lang = null) {
+    'action' => function () {
+      $products = WK1::products();
+      $productsPage = page('products');
+
+      $newProducts = [];
+      $updatedProducts = [];
+
+      foreach ($products as $index => $product) {
+
+        // if ($index > 5) {
+        //   break;
+        // }
+
+        // Only extract books, not typefaces nor stationery
+        if ($product['categories'][0]['slug'] !== 'books') {
+          continue;
+        }
+
+        // Get name
+        $title = $product['name'];
+        // Replace inline <br> with line breaks
+        $title = preg_replace('/<br\s*\/?>/', '|', $title);
+        // Remove all other HTML tags
+        $title = Str::unhtml($title);
+        // Deduplicate spaces, and remove spaces next to a pipe
+        $title = preg_replace('/\s+/', ' ', $title);
+        $title = preg_replace('/\s*\|\s*/', '|', $title);
+
+        $slug = Str::slug($title);
+        $content = [
+          'title' => $title,
+
+          'oldWeltkern' => [
+            'title' => $product['name'],
+
+            'slug' => $product['slug'],
+
+            'id' => $product['id'],
+
+            'isbn' => (function () use ($product) {
+              foreach ($product['header'][0]['header']['block_option'] as $option) {
+                if ($option['option'] === 'ISBN') {
+                  return $option['value'];
+                }
+              }
+              return 'NO ISBN';
+            })(),
+
+            'price' => $product['price'],
+
+            'weight' => $product['weight'],
+
+            'author' => (function () use ($product) {
+              $author = $product['header'][0]['header']['author_information']['author'];
+              return [
+                'name' => $author['name'],
+                'id' => $author['term_id'],
+              ];
+            })(),
+
+            'description' => $product['short_description'],
+
+            'details' => (function () use ($product) {
+              $details = '';
+
+              foreach ($product['header'][0]['header']['block_option'] as $option) {
+                $details .= '- ' . $option['option'] . ': ' . $option['value'] . PHP_EOL;
+              }
+
+              return $details;
+            })(),
+
+            'gallery' => (array_map(
+              function ($image) {
+                return [
+                  'url' => $image['url'],
+                  'id' => $image['id'],
+                ];
+              },
+              $product['gallery_image']
+            )),
+
+            'tags' => (array_map(
+              function ($tag) {
+                return [
+                  'name' => $tag['name'],
+                  'id' => $tag['id'],
+                ];
+              },
+              $product['tags']
+            )
+            )
+          ]
+        ];
+
+        if ($productPage = $productsPage->draft($slug)) {
+          $productPage = $productPage->update($content);
+          $updatedProducts[] = $productPage;
+        } else {
+          $productPage = $productsPage->createChild([
+            'slug' => $slug,
+
+            'template' => 'product_book',
+            'content' => $content
+          ]);
+          $newProducts[] = $productPage;
+        }
+      }
+
+      $returnString = '';
+
+      foreach ($newProducts as $product) {
+        $returnString .= '<pre style="color:green">ADD: ' . $product->title() . ' (' . $product->slug() . ')</pre>';
+      }
+
+      foreach ($updatedProducts as $product) {
+        $returnString .= '<pre style="color:orange">UPDATE: ' . $product->title() . ' (' . $product->slug() . ')</pre>';
+      }
+
+      return $returnString;
     }
   ],
   // Parses the imported products drafts and fills the product pages with the data
@@ -17,5 +138,5 @@ return [
     'language' => '*',
     'action' => function ($lang = null) {
     }
-  ]
+  ],
 ];
