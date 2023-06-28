d5957292e31da3ca8e3ee242971f7bafb1c3696a
apademide <contact@apademide.ch>
2023-06-28 10:08:24 +0200

    feat: add sync-weltkern routee

---
+++ settings/config/routes/sync-weltkern.php
@@ -0,0 +1,11 @@
+<?php
+
+use Kirby\Cms\Response;
+use AUAUST\products\WK1;
+
+return [
+  'pattern' => 'sync-weltkern',
+  'language' => '*',
+  'action' => function ($lang = null, $id = null) {
+  }
+];
