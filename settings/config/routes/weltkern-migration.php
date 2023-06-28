dcc74620553010dc60aefeaa56cb9dafe93cd454
apademide <contact@apademide.ch>
2023-06-28 11:15:01 +0200

    files: sync-weltkern → weltkern-migration

--- settings/config/routes/weltkern-migration.php
+++ settings/config/routes/weltkern-migration.php
@@ -6,6 +6,7 @@ use AUAUST\products\WK1;
 return [
   'pattern' => 'sync-weltkern',
   'language' => '*',
-  'action' => function ($lang = null, $id = null) {
+  'action' => function ($lang = null) {
+    return kirby()->site()->pageProducts()->toPage()->drafts()->toAlgoliaData();
   }
 ];
