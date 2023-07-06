0d320d71e75b189133063c8f0aaeb885714ae3b5
apademide <contact@apademide.ch>
2023-07-06 11:43:19 +0200

    imp: successfully show custom page

--- site/plugins/auaust-homepage/index.php
+++ site/plugins/auaust-homepage/index.php
@@ -10,12 +10,15 @@ Kirby::plugin("auaust/homepage", [
     'homepage-hero' => [
       'extends' => 'pages',
       'props' => [
-        'value' => function ($value = null) {
-          // Pretty hacky way to generate the list of tagset pages to be displayed in the field.
-          // It works by generating a YAML string of the tagset pages' UUIDs.
-          // return '- ' . kirby()->site()->find('home')->uuid()->id();
-          return kirby()->site()->find(false, false, ...[page('home')->simpleUuid()]);
+        'value' => function () {
+          return $this->toPages(
+            [
+              page('home')->toArray()
+            ]
+          );
         },
+        'disabled' => true,
+        'translate' => false,
       ],
       'save' => function () {
         // Return null so that the field is not saved.
