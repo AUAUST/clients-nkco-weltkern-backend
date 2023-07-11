7456c2f9b648978fdb30098ad7d1e1b3f3e2c70e
apademide <contact@apademide.ch>
2023-07-11 08:36:36 +0200

    files: remove blocks from homepage plugin

--- site/plugins/auaust-homepage/src/index.js
+++ site/plugins/auaust-homepage/src/index.js
@@ -1,14 +1,7 @@
-import Spacer from "./components/Spacer.vue";
-import Articles from "./components/Articles.vue";
-
 panel.plugin("auaust/homepage", {
   fields: {
     "homepage-hero": {
       extends: "k-pages-field",
     },
-  },
-  blocks: {
-    spacer: Spacer,
-    articles: Articles,
   },
 });
